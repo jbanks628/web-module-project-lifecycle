28885bedbb0fabf8e9a380a57e48c406244418d7
Jada Banks <jadabanks@Jadas-MacBook-Air.local>
2022-01-11 19:28:10 -0600

    added to App JS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+// import User from './components/User';
+// import Follower from './components/Follower';
+// import FollowerList from './components/FollowerList';
 
 class App extends React.Component {
+
+
+
   render() {
     return(<div>
-      Github Card
+      <h1>GITHUB INFO</h1>
+      <form>
+        <input
+        placeholder='Github Handle'
+        name='handle'
+        />
+        <button>Search</button>
+      </form>
+      {/* <User />
+      <Follower /> */}
     </div>);
   }
 }
