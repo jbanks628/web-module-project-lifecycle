a7394123ed0603a25447a17dd763cd64a24aa0e1
Jada Banks <jadabanks@Jadas-MacBook-Air.local>
2022-01-11 23:34:24 -0600

    everything is showing. Adding event handlers next

--- src/components/FollowerList.js
+++ src/components/FollowerList.js
@@ -2,17 +2,15 @@ import React from "react";
 import Follower from "./Follower";
 
 class FollowerList extends React.Component {
-  //   state = {
-  //       followerInfo = []
-  //   }
-
   render() {
-    // const { avatar_url, login, followerInfo } = this.props.list;
     return (
       <div>
         {this.props.list.map((item) => {
           return (
-            <Follower key={item.id} image={item.avatar_url} item={item.login} />
+            <div>
+              <img src={item.avatar_url} />
+              <Follower key={item.id} item={item.login} />
+            </div>
           );
         })}
       </div>
